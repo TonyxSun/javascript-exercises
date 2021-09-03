c2bcfdd1eeca33a27187e0cfc316a201c9cff8dc
Tony Sun <sunwuyue@live.com>
2021-09-03 11:41:54 -0700

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, removeItems) {
+    let args = Array.from(arguments);
+    // let arrLength = arr.length;
+    for (let i = arr.length-1;  i >= 0; i--){
+        for (let j = 1; j < args.length; j++){
+            if (args[j] === arr[i]){
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 module.exports = removeFromArray;
