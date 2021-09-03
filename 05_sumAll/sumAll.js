38f420e1002edfeda81cf931f0e7ae79221cb138
Tony Sun <sunwuyue@live.com>
2021-09-03 11:59:39 -0700

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, stopNum) {
+    let sum = 0;
+    if (startNum > stopNum){
+        let temp = startNum;
+        startNum = stopNum;
+        stopNum = temp;
+    }
+    if (startNum < 0 || stopNum < 0 || typeof(startNum)!= "number" || typeof(stopNum) != "number") return "ERROR";
+    for (let i = startNum; i <= stopNum; i++){
+        sum += i;    }
+    return sum;
 };
 
 module.exports = sumAll;
