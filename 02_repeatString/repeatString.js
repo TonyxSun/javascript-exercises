b31944fd0ef82dd3fd05dda2e3e919dff9ce0043
Tony Sun <sunwuyue@live.com>
2021-09-03 11:01:16 -0700

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatCount) {
+    let repeatString = "";
+    if (repeatCount < 0) return "ERROR";
+    for (let i = 0; i < repeatCount; i++){
+        repeatString += string;
+    }
+    return repeatString;
 };
 
 module.exports = repeatString;
