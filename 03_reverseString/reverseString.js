0561ba3fe1c6b4233982d6f03771cf1c128b9806
Tony Sun <sunwuyue@live.com>
2021-09-03 11:16:28 -0700

    Completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
 const reverseString = function(str) {
+    let charArray = [];
+    let reversedStr = "";
+    let strLength = str.length;
+    for (let i = 0; i < strLength; i++){
+        charArray.push(str.charAt(i));
+    }
 
+    for (let j = 0; j < strLength; j++){
+        reversedStr += charArray.pop();
+    }
+    return reversedStr;
 };
 
 module.exports = reverseString;
