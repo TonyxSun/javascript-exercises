394bffef47da1bf2ca6c38233f47e1655034dcbb
Tony Sun <sunwuyue@live.com>
2021-09-06 14:43:35 -0700

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+let findTheOldest = function (people) {
+    const oldest = people.reduce((a, b) => {
+        let aDeathYear = a.yearOfDeath? a.yearOfDeath : (new Date()).getFullYear();
+        let bDeathyear = b.yearOfDeath? b.yearOfDeath : (new Date()).getFullYear();
+        aAge = aDeathYear - a.yearOfBirth;
+        bAge = bDeathyear - b.yearOfBirth;
+        if (aAge > bAge) {
+             return a;
+    } else return b;})
+    return oldest;
 };
 
 module.exports = findTheOldest;
