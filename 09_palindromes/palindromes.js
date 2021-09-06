ad4667b9a7176e7c5212e6a786be72793ca2b696
Tony Sun <sunwuyue@live.com>
2021-09-06 13:53:16 -0700

    finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    
+    let reverseString = "";
+    let newString = "";
+    let punctuations = "., !";
+    const strLength = string.length;
+    for (let i = 0; i < strLength; i++){
+        if (!punctuations.includes(string[i])) {
+            newString += string[i].toLowerCase();
+        }
+    }
+    for (let i = newString.length -1; i >= 0;i--){ 
+        reverseString += (newString[i]);
+    }
+    return (reverseString === newString);
 };
 
 module.exports = palindromes;
